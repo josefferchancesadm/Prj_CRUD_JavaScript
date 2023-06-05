677e3549473cac874349f0052b2df9daabcff331
josefferchancesadm <josefferchancesadm@gmail.com>
2023-06-05 16:25:09 -0300

    views

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("ola mundo");
+const express = require('express');
+// const session = require('express-session')
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+var path = require('path');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+// app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join())
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World para docker testes agora');
+});
+
+app.listen(PORT, HOST, ()=> {
+    console.log(`Servidor rodando na porta ${PORT} no host ${HOST}`)
+})
